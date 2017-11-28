c1af5ea710b489ebceca82c556618936cd9200e5
aroselee45 <31043381+aroselee45@users.noreply.github.com>
2017-11-28 09:29:03 -0800

    Initial commit

---
+++ Notes.py
@@ -0,0 +1,39 @@
+print("Hello world")
+
+# Autumn
+
+a = 4
+b = 3
+
+print(3 + 5)
+print(5 - 3)
+print(3 * 5)
+print(6 / 2)
+print(3 ** 2)
+
+print()
+print("Try to figure out how this works")
+print( 15 % 12 )
+
+# the "%" sign is a modulus. It finds the remainder
+
+car_name = "The Wiebe Moble"
+car_type = "BMW"
+car_cylinders = 8
+car_mpg = 5000.9
+
+print("I have a car called %s. It's pretty nice." % car_name)
+print("I have a car called %s. It's a %s." % (car_name, car_type)) # watch the order
+
+# Here is where we get a little fancy
+print("What is your name?")
+name = input(">_ ")
+print("Hello %s." % name)
+
+input("How old are you?")
+print("%s?! That's really old. You belong in a retirement home")
+
+
+
+
+
