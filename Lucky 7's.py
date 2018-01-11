e1dc674b31615059f24dc48087374d87aa8447e0
aroselee45 <31043381+aroselee45@users.noreply.github.com>
2018-01-11 09:45:11 -0800

    money print

--- Lucky 7's.py
+++ Lucky 7's.py
@@ -6,6 +6,18 @@ dice2 = random.randint(1, 6)
 print(dice1)
 print(dice2)
 
+total = dice1 + dice2
+print(total)
+
+money = 15
+
+if total == 7:
+    money += 4
+
+else:
+    total != 7:
+    money -=1
+print(money)
 
 
 
